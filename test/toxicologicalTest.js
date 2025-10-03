fc113c7f7cbad6aafce6526a46ee34122809db72
Dhiego Borba <dhiego.borba@px.center>
2025-10-03 09:11:19 -0300

    feat: alterando estrutura

--- test/toxicologicalTest.js
+++ test/toxicologicalTest.js
@@ -1,21 +1,16 @@
 const fs = require('fs');
 const path = require('path');
 const ToxicologicalScraper = require('../src/scrapers/ToxicologicalScraper');
-const toxicologicalScraper = new ToxicologicalScraper({ maxConcurrency: 5 });
+const drivers = require('./data/drivers.json');
 
 (async () => {
   console.log('--- Iniciando processo de consulta em lote ---');
 
-  const caminhoMotoristas = path.resolve(__dirname, 'data/drivers.json');
-  if (!fs.existsSync(caminhoMotoristas)) {
-    console.error('Arquivo drivers.json não encontrado!');
+  const toxicologicalScraper = new ToxicologicalScraper({ maxConcurrency: 5 });
 
-    process.exit(1);
+  for await (const result of toxicologicalScraper.processBatch(drivers)) {
+    console.log(result);
   }
 
-  const motoristas = JSON.parse(fs.readFileSync(caminhoMotoristas, 'utf8'));
-
-  for await (const resultado of toxicologicalScraper.processBatch(motoristas)) {
-    console.log('Retorno: ', resultado);
-  }
+  console.log('--- Finalizou processo de consulta em lote ---');
 })();
