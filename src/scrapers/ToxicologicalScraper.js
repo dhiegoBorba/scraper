76d39af18ab6b935499b1053b0d74e16fdf36c87
Dhiego Borba <dhiego.borba@px.center>
2025-10-08 08:56:32 -0300

    ajuste user agent

--- src/scrapers/ToxicologicalScraper.js
+++ src/scrapers/ToxicologicalScraper.js
@@ -247,9 +247,17 @@ class ToxicologicalScraper {
 
   async #openPage(browserContext) {
     const page = await browserContext.newPage();
-    await page.setUserAgent(
-      'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-    );
+
+    const userAgents = [
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.6613.119 Safari/537.36',
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0',
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.6 Safari/605.1.15',
+      'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
+      'Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1',
+    ];
+
+    await page.setUserAgent(userAgents[Math.floor(Math.random() * userAgents.length)]);
+
     await page.setViewport({ width: 1920, height: 1080 });
     await this.#setupPage(page);
     await page.goto(
